c3b782579195e76347ace710ffc772ae30927db3
Tanisha <jaiswaltanisha157@gmail.com>
2025-08-24 02:58:24 +0530

    Done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,22 +1,22 @@
-// routes/auth.js
 import express from "express";
 import passport from "passport";
 import { login, logout, signup, googleAuthCallback } from "../controllers/authControllers.js";
 
-const router = express.Router(); //router is an instance of express.Router that we use to hold all our routes 
+const router = express.Router();
 
 router.post("/login", login);
 router.post("/register", signup);
 router.post("/logout", logout);
 
-// ✅ Google auth routes
+// Google auth routes
 router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 
 router.get(
   "/google/callback",
-  passport.authenticate("google", { failureRedirect: "http://localhost:3000/login" }),
-  googleAuthCallback //function from 
-    // authControllers.js to handle successful login
+  passport.authenticate("google", {
+    failureRedirect: `${process.env.CLIENT_URL}/login`, 
+  }),
+  googleAuthCallback
 );
 
 export default router;
